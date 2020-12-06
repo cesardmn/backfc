a1e205613e2c25f2c5e71dfae6b00423ca801a9b
Cesar <cesardmn@gmail.com>
2020-12-05 23:20:21 -0300

    cors update

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
-    'https://www.fogaocaseiro.com.br',
+    'www.fogaocaseiro.com.br',
     'http://192.168.0.16:3000'
 ]
 
