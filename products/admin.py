d87291e432b3b146671e3609da2bc5acbe03bec0
Cesar <cesardmn@gmail.com>
2020-12-23 20:57:49 -0300

    create new models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+import locale
 from django.forms.widgets import Textarea
-from .models import *
+from .models import Item, ItemGroup, ItemType
 
 
 def ativar(modeladmin, request, queryset):
@@ -14,110 +15,58 @@ def desativar(modeladmin, request, queryset):
         item.active = False
         item.save()
 
+
 def ativar_desconto(modeladmin, request, queryset):
     for item in queryset:
         item.discount = True
         item.save()
+
 
 def desativar_desconto(modeladmin, request, queryset):
     for item in queryset:
         item.discount = False
         item.save()
 
-@admin.register(Combo)
-class ComboAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'acompanhamentos',
-        'carnes',
-        'frangos',
-        'massas',
-        'peixe',
-        'active'
-    )
 
-    list_filter = ('active',)
-
-    ordering = ('id',)
-
-    actions = (
-        ativar,
-        desativar
-    )
+@admin.register(ItemGroup)
+class ItemGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', )
 
 
-@admin.register(Fit)
-class FitAdmin(admin.ModelAdmin):
+@admin.register(ItemType)
+class ItemTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'packge_amount')
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
     list_display = (
         'id',
-        'name',
-        'price',
-        'sale',
         'active',
-        'discount'
-    )
-
-    list_display_links = ('id', 'name', 'price')
-
-    list_filter = ('active', 'discount',)
-
-    ordering = ('id',)
-
-    actions = (ativar, desativar, ativar_desconto, desativar_desconto)
-
-    formfield_overrides = {
-        models.TextField: {
-            'widget': Textarea(attrs={'rows': 2, 'cols': 45})
-        },
-    }
-
-
-@admin.register(LowCarb)
-class LowCarbAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'name',
-        'price',
-        'active',
-        'discount'
-    )
-
-    list_display_links = ('id', 'name', 'price')
-
-    list_filter = ('active', 'discount',)
-
-    ordering = ('id',)
-
-    actions = (ativar, desativar, ativar_desconto, desativar_desconto)
-
-    formfield_overrides = {
-        models.TextField: {
-            'widget': Textarea(attrs={'rows': 2, 'cols': 45})
-        },
-    }
-
-
-@admin.register(Portion)
-class PortionAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
+        'description',
         'type',
-        'name',
-        'price',
-        'active',
-        'discount'
+        'group',
+        'full_price_format',
+        'sale_price_format',
+        'discount',
     )
 
-    list_display_links = ('id', 'type','name', 'price')
+    list_display_links = ('id', 'description')
 
-    list_filter = ('type', 'active', 'discount',)
-
-    ordering = ('id',)
+    ordering = ('group', 'type', 'id')
 
     actions = (ativar, desativar, ativar_desconto, desativar_desconto)
 
-    formfield_overrides = {
-        models.TextField: {
-            'widget': Textarea(attrs={'rows': 2, 'cols': 45})
-        },
-    }
+    list_filter = ('group', 'type', 'active')
+
+    search_fields = ('description', )
+
+    def full_price_format(self, obj):
+        locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+        return locale.currency(obj.full_price)
+    full_price_format.short_description = 'preço cheio'
+
+    def sale_price_format(self, obj):
+        locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+        return locale.currency(obj.sale_price)
+    sale_price_format.short_description = 'preço de venda'
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import BooleanField
 
 
 class Fit(models.Model):
@@ -143,3 +144,91 @@ class Combo(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ItemGroup(models.Model):
+    name = models.CharField(
+        max_length=20,
+        verbose_name='grupo',
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Grupo'
+        verbose_name_plural = 'Grupos'
+
+
+class ItemType(models.Model):
+    name = models.CharField(
+        max_length=20,
+        verbose_name='tipo',
+        unique=True
+    )
+
+    packge_amount = models.PositiveSmallIntegerField(
+        verbose_name='embalagem (ml)'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+
+
+class Item(models.Model):
+
+    group = models.ForeignKey(
+        ItemGroup,
+        on_delete=models.CASCADE,
+        verbose_name='grupo'
+    )
+
+    type = models.ForeignKey(
+        ItemType,
+        on_delete=models.CASCADE,
+        verbose_name='tipo'
+    )
+
+    description = models.CharField(
+        max_length=100,
+        verbose_name='descrição'
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        verbose_name='preço'
+    )
+
+    active = BooleanField(
+        default=False,
+        verbose_name='ativo',
+    )
+
+    discount = BooleanField(
+        default=False,
+        verbose_name='desconto (10%)'
+    )
+
+    @property
+    def full_price(self):
+        return float(self.price)
+
+    @property
+    def sale_price(self):
+        if self.discount == True:
+            return float(self.price) * 0.9
+        else:
+            return float(self.price)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Itens'
