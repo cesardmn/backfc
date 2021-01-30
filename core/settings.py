dbcc6abca19bbcf1ceffd0d6b6fc978a83997fff
Cesar <cesardmn@gmail.com>
2021-01-30 11:41:15 -0300

    hot-fix staticfiles

--- core/settings.py
+++ core/settings.py
@@ -17,11 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'products',
     'rest_framework',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
